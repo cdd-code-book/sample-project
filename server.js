c05bf9967f37e7107a4c457660906aa267007dec
cdd-code-book <cdd-code-book@users.noreply.github.com>
2015-03-06 14:08:55 +0000

    Server now reports its own version, so that automatic codeship.com deployment to Heroku can be verified.

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
-var express = require('express');
+var express = require('express'),
+    packageManifest = require('./package.json');
+
 var app = express();
 
 app.set('port', process.env.PORT || 2001);
  
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Hello World! Running server version: ' + packageManifest.version);
 });
  
 app.listen(app.get('port'), function() {
-  console.log('Server is up and running on port: ', app.get('port'));
+  console.log('Server is up and running on port:', app.get('port'));
 });
