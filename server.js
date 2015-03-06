9a0736b5e89848fcda5fee64a8c9b51533e721a8
cdd-code-book <cdd-code-book@users.noreply.github.com>
2015-03-06 13:56:46 +0000

    Read port no. from environment variable PORT to support integration with Heroku.

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var app = express();
+
+app.set('port', process.env.PORT || 2001);
  
 app.get('/', function (req, res) {
-  res.send('Hello World');
+  res.send('Hello World!');
 });
  
-app.listen(2001);
+app.listen(app.get('port'), function() {
+  console.log('Server is up and running on port: ', app.get('port'));
+});
