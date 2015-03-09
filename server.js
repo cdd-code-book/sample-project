5a41dde239d15d8beeaaf22124bc216cac548405
cdd-code-book <cdd-code-book@users.noreply.github.com>
2015-03-09 14:59:12 +0000

    Server now returns different responses for different users, based on IP address.

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 var express = require('express'),
+    cidrMatcher = require('cidr_match'),
     packageManifest = require('./package.json');
 
 var app = express();
 
 app.set('port', process.env.PORT || 2001);
- 
-app.get('/', function (req, res) {
-  res.send('Hello World! Running server version: ' + packageManifest.version);
+
+var SUPER_ADMIN_IP_ADDRESS_RANGE = '31.221.0.0/16';
+
+app.get('/', function (request, res) {
+  var ip = request.headers['x-forwarded-for'] || request.connection.remoteAddress;
+
+  var userIsSuperAdmin = cidrMatcher.cidr_match(ip, SUPER_ADMIN_IP_ADDRESS_RANGE);
+
+  if (userIsSuperAdmin) {
+    res.send('Hello Super Admin! Running server version: ' + packageManifest.version);
+  } else {
+    res.send('Hello World!');
+  }
 });
- 
+
 app.listen(app.get('port'), function() {
   console.log('Server is up and running on port:', app.get('port'));
 });
