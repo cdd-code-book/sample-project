d55b611dfb4bfd3cf94fdd915e9b50376828d42c
cdd-code-book <cdd-code-book@users.noreply.github.com>
2015-07-07 17:21:32 +0100

    Changed Datadog agent provisioning script to actually install it instead of just downloading the script, plus added signature verification of the script.

--- build/packer/scripts/monitoring/install-datadog-agent.sh
+++ build/packer/scripts/monitoring/install-datadog-agent.sh
@@ -1,13 +1,22 @@
 #!/bin/bash -e
 
+SHASUM=c820160e728188cb915a8248707128f83efa1668c66c8b275d7240bcc1a60f5f
+
 if [ -z "$DD_API_KEY" ] ; then
   echo "Environment variable DD_API_KEY is not defined, skipping agent install"
   exit 0
 fi
 
-set -e
+set -ex
 
 apt-get update -yqq
-apt-get install -yqq curl
+apt-get install -yqq wget
 
-curl -L https://raw.githubusercontent.com/DataDog/dd-agent/master/packaging/datadog-agent/source/install_agent.sh
+wget https://raw.githubusercontent.com/DataDog/dd-agent/master/packaging/datadog-agent/source/install_agent.sh -O install_dd_agent.sh
+
+echo "Verifying download integrity..."
+sha256sum install_dd_agent.sh | grep -q "$SHASUM  install_dd_agent.sh"
+
+chmod +x install_dd_agent.sh
+./install_dd_agent.sh
+rm install_dd_agent.sh
